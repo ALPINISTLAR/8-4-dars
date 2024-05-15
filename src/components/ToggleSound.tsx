7f5b7d5d962e25f8ca75fd5ee8db97899a44445f
ALPINISTLAR <niveam424@gmail.com>
2024-05-15 14:41:10 +0200

    ToggleSound component

---
+++ src/components/ToggleSound.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ToggleSound.css';
+
+interface ToggleSoundProps {
+  sound: boolean;
+  toggleSound: () => void;
+}
+
+const ToggleSound: React.FC<ToggleSoundProps> = ({ sound, toggleSound }) => (
+  <button
+    className={`ToggleSound ${sound ? 'active' : ''}`}
+    onClick={toggleSound}
+    title={sound ? 'Disable Sound' : 'Enable Sound'}
+  >
+    <i className={`fa fa-volume-${sound ? 'up' : 'mute'}`} />
+  </button>
+);
+
+export default React.memo(ToggleSound);
